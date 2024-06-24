976ebe9da31c416e963423aed5e03284681c4f4b
Chris Ehigimetor <bishopsirhchris@gmail.com>
2024-06-24 17:23:19 +0100

    --wip--

---
+++ src/main/java/com/ghost/payment_api_gateway/PaymentApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ghost.payment_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ghost/payment_api_gateway/PaymentApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghost.payment_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
